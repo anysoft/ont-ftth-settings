f0e90837d910b769c8d48c0cc4d29a9239560978
anysoft <anysoft@yeah.net>
2024-01-17 15:00:54 +0800

    update openwrt scripts and add a demo of openwrt (not finish)

---
+++ arm_tools/dropbear/self-compiled/bin/createkey.sh
@@ -0,0 +1,4 @@
+./dropbearkey -t dss -f /etc/dropbear/dropbear_dss_host_key
+./dropbearkey -t rsa -f /etc/dropbear/dropbear_rsa_host_key
+./dropbearkey -t ecdsa -f /etc/dropbear/dropbear_ecdsa_host_key
+./dropbearkey -t ed25519 -f /etc/dropbear/dropbear_ed25519_host_key
